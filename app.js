5d0c98da520816d186a1c0bd0e8600ad45857627
LokeshS571 <lokeshv542@gmail.com>
2017-03-26 13:52:46 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+var Employee= {name:"Lokesh S",age:21,salary:"3.3 lpa",address: {city:"Bengaluru", state:"Karnataka",pincode:560040}};
+// to display in console
+console.log("Employee Name :",Employee.name); 
+console.log("Age :",Employee.age);
+console.log("Salary :",Employee.salary);
+console.log("City :",Employee.address.city);
+console.log("State :",Employee.address.state);
+console.log("Pincode :",Employee.address.pincode);
